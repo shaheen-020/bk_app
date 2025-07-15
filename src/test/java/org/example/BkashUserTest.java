ec92257a45d8f3676d8a39f6d5f2d706b32b89ab
shahinur69 <shahinurmubarak238@gmail.com>
2025-07-15 22:26:35 +0600

    testing testTimeOut() using assertTimeOut in BkashUser

--- src/test/java/org/example/BkashUserTest.java
+++ src/test/java/org/example/BkashUserTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BkashUserTest {
@@ -11,5 +13,12 @@ class BkashUserTest {
         user.cashIn(500, "1234");
         assertEquals(500.0, user.getBalance());
     }
-
+    @Test
+    public void testTimeOut(){
+        BkashUser user = new BkashUser("01792345238", "1234");
+        user.cashIn(10000, "1234");
+        assertTimeout(Duration.ofMillis(100), () ->{
+            user.sendMoney("01792345238", 100, "1234");
+        });
+    }
 }
